4d41e240cc92cf2933b9fec7dc16eb15299db3e1
gpwlfmfwlzuwnj-ui <gpwlfmfwlzuwnj@gmail.com>
2026-04-27 10:32:38 +0900

    04/27

--- Ch06/Assets/20.Scripts/PlayerController.cs
+++ Ch06/Assets/20.Scripts/PlayerController.cs
@@ -3,22 +3,26 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     float jumpForce = 400f;
-    float walkForce = 30f;
+    public float walkForce = 7f;
     float maxwalkSpeed = 1f;
+    Animatior anim;
 
     public Sprite[] walkSprites;
-    public float animationPeriod = 0.1f;
+    public Sprite jumpSprite;
+    public float animationPeriod = 0.2f;
     float time = 0;
     int idx = 0;
     SpriteRenderer sr;
 
     Rigidbody2D rb;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Application.targetFrameRate = 60;
         rb = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -36,11 +40,33 @@ public class PlayerController : MonoBehaviour
         }
 
         time += Time.deltaTime;
-        if(time > animationPeriod)
+
+        if (rb.linearVelocityY != 0)
+        {
+            anim.SetBool("IsJumping", false);
+        }
+        else if (time > animationPeriod)
         {
             time = 0;
-            sr.sprite = walkSprites[idx];
-            idx = 1 - idx;
         }
     }
+        //if (rb.linearVelocityY != 0)
+        //{
+        //    sr.sprite = jumpSprite;
+        //}
+        //else if (time > animationPeriod)
+        //{
+        //    time = 0;
+        //    sr.sprite = walkSprites[idx];
+        //    idx++;
+        //    if (idx == walkSprites.Length)
+        //    {
+        //        idx = 0;
+        //    }
+        //}
+
+        private void OnTriggerEnter(Collider other)
+        {
+    Debug.Log("¼º°ø");
+        }
 }
