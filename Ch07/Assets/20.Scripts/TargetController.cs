91d111a2806a1ccd45a6461fbc18896cf516a90e
gpwlfmfwlzuwnj-ui <gpwlfmfwlzuwnj@gmail.com>
2026-05-11 10:53:24 +0900

    05/11

---
+++ Ch07/Assets/20.Scripts/TargetController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class TargetController : MonoBehaviour
+{
+    GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt("player.transform");
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+    }
+}
